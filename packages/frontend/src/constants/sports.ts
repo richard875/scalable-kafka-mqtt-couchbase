911412f1de383e006e36f82b948e5955613acf67
Richard Everley <hello@richardeverley.com>
2025-09-10 08:26:45 +1000

    - Feature: Add `urlKey` to Sport type and rename mock API files

--- packages/frontend/src/constants/sports.ts
+++ packages/frontend/src/constants/sports.ts
@@ -6,6 +6,7 @@ import soccer from "@/assets/icons/soccer.svg";
 
 export type Sport = {
   key: string;
+  urlKey: string;
   titleShort: string;
   title: string;
   icon: string;
@@ -14,36 +15,42 @@ export type Sport = {
 const Sports: Sport[] = [
   {
     key: "premierLeague",
+    urlKey: "soccer_epl",
     titleShort: "Premier League",
     title: "English Premier League",
     icon: soccer,
   },
   {
     key: "nfl",
+    urlKey: "americanfootball_nfl",
     titleShort: "NFL",
     title: "National Football League",
     icon: football,
   },
   {
     key: "mlb",
+    urlKey: "baseball_mlb",
     titleShort: "MLB",
     title: "Major League Baseball",
     icon: baseball,
   },
   {
     key: "nrl",
+    urlKey: "rugbyleague_nrl",
     titleShort: "NRL",
     title: "National Rugby League",
     icon: rugby,
   },
   {
     key: "afl",
+    urlKey: "aussierules_afl",
     titleShort: "AFL",
     title: "Australian Football League",
     icon: aussie,
   },
   {
     key: "mls",
+    urlKey: "soccer_usa_mls",
     titleShort: "MLS",
     title: "Major League Soccer",
     icon: soccer,
