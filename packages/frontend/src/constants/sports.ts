55fd44cd1217952bea3661e62ad37bce2f7da8aa
Richard Everley <hello@richardeverley.com>
2025-09-10 00:01:39 +1000

    - Feature: Add sports constant data

---
+++ packages/frontend/src/constants/sports.ts
@@ -0,0 +1,46 @@
+import aussie from "@/assets/icons/aussie.svg";
+import baseball from "@/assets/icons/baseball.svg";
+import football from "@/assets/icons/football.svg";
+import rugby from "@/assets/icons/rugby.svg";
+import soccer from "@/assets/icons/soccer.svg";
+
+const Sports = [
+  {
+    key: "premierLeague",
+    titleShort: "EPL",
+    title: "Premier League",
+    icon: soccer,
+  },
+  {
+    key: "nfl",
+    titleShort: "NFL",
+    title: "National Football League",
+    icon: football,
+  },
+  {
+    key: "mlb",
+    titleShort: "MLB",
+    title: "Major League Baseball",
+    icon: baseball,
+  },
+  {
+    key: "nrl",
+    titleShort: "NRL",
+    title: "National Rugby League",
+    icon: rugby,
+  },
+  {
+    key: "afl",
+    titleShort: "AFL",
+    title: "Australian Football League",
+    icon: aussie,
+  },
+  {
+    key: "mls",
+    titleShort: "MLS",
+    title: "Major League Soccer",
+    icon: soccer,
+  },
+];
+
+export default Sports;
