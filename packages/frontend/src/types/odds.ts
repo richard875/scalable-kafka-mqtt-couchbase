71cc00e5b11a80de2d98e47966602da48d65dd28
Richard Everley <hello@richardeverley.com>
2025-09-09 23:49:12 +1000

    - Feature: Add Odds type

---
+++ packages/frontend/src/types/odds.ts
@@ -0,0 +1,25 @@
+export type Odds = Array<{
+  id: string;
+  sport_key: string;
+  sport_title: string;
+  commence_time: string;
+  home_team: string;
+  away_team: string;
+  bookmakers: Array<{
+    key: string;
+    title: string;
+    last_update: string;
+    markets: Array<{
+      key: string;
+      last_update: string;
+      outcomes: Array<{
+        name: string;
+        price: number;
+      }>;
+      link: string;
+      sid: string;
+    }>;
+    link: string;
+    sid: string;
+  }>;
+}>;
