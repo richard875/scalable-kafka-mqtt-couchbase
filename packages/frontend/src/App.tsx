c1b7d5551376a5327bdbac0cde1c3d479e38e471
Richard Everley <hello@richardeverley.com>
2025-09-10 08:12:20 +1000

    - Styling: Adjust margin

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
     <div className="flex flex-col min-h-screen">
       <Header />
       <div className="relative flex-auto main-wrapper">
-        <div className="max-w-[1664px] m-auto my-4 px-4 flex gap-4">
+        <div className="max-w-[1664px] m-auto mt-4 mb-1 px-4 flex gap-4">
           <div className="w-55">
             <SideBar />
           </div>
