e021e1fc538d2047f36382e086a601df77b782c9
Richard Everley <hello@richardeverley.com>
2025-09-10 01:17:30 +1000

    - Styling: Add `font-smoothing` CSS rules

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <div className="relative flex-auto">
+      <div className="relative flex-auto main-wrapper">
         <div className="max-w-[1664px] m-auto my-4 px-4 flex gap-4">
           <div className="w-55">
             <SideBar />
