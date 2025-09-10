d05f29841227c150775106706dd6d19352e65714
Richard Everley <hello@richardeverley.com>
2025-09-11 05:30:37 +1000

    - Feature: Add logic for confirming bets

--- packages/frontend/src/components/betslip/Confirm.tsx
+++ packages/frontend/src/components/betslip/Confirm.tsx
@@ -1,13 +1,30 @@
 import Decimal from "decimal.js";
+import { toast } from "react-toastify";
 import useSportStore from "@/store/sportStore";
+import formatAmount from "@/helper/formatAmount";
 
 const Confirm = () => {
-  const { selectedBets } = useSportStore();
+  const { selectedBets, clearSelectedBets } = useSportStore();
   const totalStake = selectedBets.reduce((acc, bet) => acc.plus(bet.amount || 0), new Decimal(0));
   const potentialPayout = selectedBets.reduce(
     (acc, bet) => acc.plus(new Decimal(bet.amount || 0).mul(new Decimal(bet.price || 0))),
     new Decimal(0)
   );
+
+  const handleClick = () => {
+    selectedBets.forEach((bet, index) => {
+      const message = `Placed $${formatAmount(bet.amount || 0)} bet on ${bet.name} @ ${formatAmount(bet.price || 0)}`;
+      const className =
+        "!rounded-xs !bg-[#222222] shadow-2xl !text-white !text-sm !font-bold border-l-4 border-[#ffe91f] font-smoothing";
+
+      setTimeout(
+        () => toast(message, { closeButton: false, hideProgressBar: true, className }),
+        500 * index
+      );
+    });
+
+    clearSelectedBets();
+  };
 
   return (
     <div className={`p-2 pb-3 border-t border-[#333333] ${selectedBets.length < 2 && "pt-3"}`}>
@@ -27,6 +44,7 @@ const Confirm = () => {
           disabled={
             selectedBets.length === 0 || selectedBets.some(bet => !bet.amount || bet.amount <= 0)
           }
+          onClick={handleClick}
           className="w-43.5 h-14 flex items-center justify-center rounded-sm text-black text-base font-normal bg-[#ffe91f] cursor-pointer disabled:opacity-50 disabled:cursor-auto"
         >
           Place bet
