87066459d4d4901fc9b0b353d242c4c96e382ee8
Richard Everley <hello@richardeverley.com>
2025-09-10 14:44:01 +1000

    - Feature: Pass `groupedOdds` into `EventCard` component

--- packages/frontend/src/components/mainContent/MainContent.tsx
+++ packages/frontend/src/components/mainContent/MainContent.tsx
@@ -1,15 +1,19 @@
 import TitleCard from "./TitleCard";
 import EventCard from "./EventCard";
+import useSportStore from "@/store/sportStore";
 
 const MainContent = () => {
+  const { groupedOdds } = useSportStore();
+
   return (
     <>
       <TitleCard />
       <div className="mt-5 mb-4 select-none">
         <span className="text-[#555555] text-base font-medium">Live & Upcoming</span>
       </div>
-      <EventCard />
-      <EventCard />
+      {groupedOdds.map(group => (
+        <EventCard key={group.date} group={group} />
+      ))}
     </>
   );
 };
