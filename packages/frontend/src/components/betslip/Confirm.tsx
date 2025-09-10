199de7f384007f7fa58dcbb502d3376ca31b50b5
Richard Everley <hello@richardeverley.com>
2025-09-11 02:49:35 +1000

    - Feature: Add dynamic data to `Confirm.tsx ` component

--- packages/frontend/src/components/betslip/Confirm.tsx
+++ packages/frontend/src/components/betslip/Confirm.tsx
@@ -1,14 +1,27 @@
+import Decimal from "decimal.js";
+import useSportStore from "@/store/sportStore";
+
 const Confirm = () => {
+  const { selectedBets } = useSportStore();
+  const totalStake = selectedBets.reduce((acc, bet) => acc.plus(bet.amount || 0), new Decimal(0));
+  const potentialPayout = selectedBets.reduce(
+    (acc, bet) => acc.plus(new Decimal(bet.amount || 0).mul(new Decimal(bet.price || 0))),
+    new Decimal(0)
+  );
+
   return (
-    <div className="p-2 pb-3 border-t border-[#333333]">
-      <div className="flex items-center justify-between">
-        <span className="text-[#888888] text-xs font-normal">Total stake:</span>
-        <span className="text-white text-base font-normal">$0.00</span>
-      </div>
+    <div className={`p-2 pb-3 border-t border-[#333333] ${selectedBets.length < 2 && "pt-3"}`}>
+      {selectedBets.length > 1 && (
+        <div className="flex items-center justify-between">
+          <span className="text-[#888888] text-xs font-normal">Total stake:</span>
+          <span className="text-white text-base font-normal">${totalStake.toFixed(2)}</span>
+        </div>
+      )}
+
       <div className="flex items-end justify-between">
         <div className="flex flex-col">
           <span className="text-[#888888] text-xs font-normal">Potential payout:</span>
-          <span className="text-white text-base font-normal">$0.00</span>
+          <span className="text-white text-base font-normal">${potentialPayout.toFixed(2)}</span>
         </div>
         <button className="w-43.5 h-14 flex items-center justify-center rounded-sm text-black text-base font-normal bg-[#ffe91f] cursor-pointer">
           Place bet
