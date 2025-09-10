000aa3afdaec0e7ce89d758e2283b46164e7cbc3
Richard Everley <hello@richardeverley.com>
2025-09-11 02:02:21 +1000

    - Feature: Add dynamic data to `Header.tsx` component

--- packages/frontend/src/components/betslip/Header.tsx
+++ packages/frontend/src/components/betslip/Header.tsx
@@ -1,9 +1,15 @@
+import useSportStore from "@/store/sportStore";
 import chevron from "@/assets/icons/chevron.svg";
 
 const Header = () => {
+  const { selectedBets } = useSportStore();
+
   return (
     <div className="w-full h-13 bg-[#333333] px-4 flex items-center justify-between cursor-pointer">
-      <span className="text-[#999999] text-base font-bold">Singles (1) @ 1.98</span>
+      <span className="text-[#999999] text-base font-bold">
+        Singles ({selectedBets.length})
+        {selectedBets.length === 1 ? <> @ {selectedBets[0].price}</> : null}
+      </span>
       <img src={chevron} alt="chevron" className="w-5 h-5" />
     </div>
   );
