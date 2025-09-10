2f0a1c83130f4dc3e67afe11effc19c8b360c90d
Richard Everley <hello@richardeverley.com>
2025-09-10 14:25:30 +1000

    - Feature: Add `odds`, `groupedOdds`, and `setOdds` states in zustand

--- packages/frontend/src/store/sportStore.ts
+++ packages/frontend/src/store/sportStore.ts
@@ -1,14 +1,28 @@
 import { create } from "zustand";
-import Sports, { type Sport } from "@/constants/sports";
+import Sports from "@/constants/sports";
+import type { Odds } from "@/types/odds";
+import type { Sport } from "@/types/sport";
+import type { GoupedOdds } from "@/types/groupedOdds";
+import transformEvents from "@/helper/transformEvents";
 
 type SportStore = {
   sport: Sport;
+  odds: Odds[];
+  groupedOdds: GoupedOdds[];
   setSport: (sport: Sport) => void;
+  setOdds: (sport: Sport) => Promise<void>;
 };
 
 const useSportStore = create<SportStore>(set => ({
   sport: Sports[0],
+  odds: [],
+  groupedOdds: [],
   setSport: (sport: Sport) => set(() => ({ sport })),
+  setOdds: async (sport: Sport) => {
+    const odds: Odds[] = await fetch(`/mock/${sport.urlKey}.json`).then(res => res.json());
+    const groupedOdds: GoupedOdds[] = transformEvents(odds);
+    set(() => ({ odds, groupedOdds }));
+  },
 }));
 
 export default useSportStore;
