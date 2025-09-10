7856d1fddcce43104180d8aaa31955d4587b7b37
Richard Everley <hello@richardeverley.com>
2025-09-11 02:09:22 +1000

    - Feature: Add dynamic data to `BetItem` component

--- packages/frontend/src/components/betslip/BetItem.tsx
+++ packages/frontend/src/components/betslip/BetItem.tsx
@@ -1,26 +1,31 @@
 import close from "@/assets/icons/close.svg";
+import useSportStore from "@/store/sportStore";
+import type { SlipItem } from "@/types/slipItem";
 
-const BetItem = () => {
+const BetItem = ({ bet }: { bet: SlipItem }) => {
+  const { setSelectedBet } = useSportStore();
+
   return (
     <div className="w-full flex border-t border-[#333333]">
-      <div className="w-9 flex items-center justify-center border-r border-[#333333] cursor-pointer">
+      <div
+        onClick={() => setSelectedBet(bet)}
+        className="w-9 flex items-center justify-center border-r border-[#333333] cursor-pointer"
+      >
         <img src={close} alt="Close" className="w-5.75 h-5.75" />
       </div>
       <div className="flex-1 px-2.5 py-2 flex justify-between">
         <div className="flex flex-col">
           <div className="mt-1 mb-1 flex flex-col">
-            <span className="text-white text-xs font-bold">Brisbane</span>
+            <span className="text-white text-xs font-bold">{bet.name}</span>
             <div className="flex w-fit h-fit bg-[#00953a] rounded-xs mb-0.5">
               <span className="uppercase text-white text-[10px] font-normal px-0.5">Cash Out</span>
             </div>
             <span className="text-[#999999] text-xs font-normal">Including Overtime</span>
           </div>
-          <span className="text-white text-xs font-normal cursor-pointer">
-            Brisbane - Gold Coast
-          </span>
+          <span className="text-white text-xs font-normal cursor-pointer">{bet.team}</span>
         </div>
         <div className="flex flex-col items-end">
-          <span className="mr-1 mb-1.25 text-white text-sm font-bold">1.55</span>
+          <span className="mr-1 mb-1.25 text-white text-sm font-bold">{bet.price}</span>
           <input
             type="number"
             placeholder="0.00"
--- packages/frontend/src/components/betslip/Betslip.tsx
+++ packages/frontend/src/components/betslip/Betslip.tsx
@@ -1,12 +1,17 @@
 import Header from "./Header";
 import BetItem from "./BetItem";
 import Confirm from "./Confirm";
+import useSportStore from "@/store/sportStore";
 
 const Betslip = () => {
+  const { selectedBets } = useSportStore();
+
   return (
     <div className="w-95 fixed bottom-0 right-10 bg-[#222222] shadow-2xl shadow-gray-900 border-t-4 border-[#ffe91f] font-smoothing">
       <Header />
-      <BetItem />
+      {selectedBets.map(bet => (
+        <BetItem key={bet.id} bet={bet} />
+      ))}
       <Confirm />
     </div>
   );
