287ebe287f9cc70fe3227aa3d5a58d6b7f4ea5df
Richard Everley <hello@richardeverley.com>
2025-09-10 14:15:38 +1000

    - Feature: Add `transformEvents` helper function

---
+++ packages/frontend/src/helper/transformEvents.ts
@@ -0,0 +1,30 @@
+import type { Odds } from "@/types/odds";
+import type { GoupedOdds } from "@/types/groupedOdds";
+
+const transformEvents = (data: Odds): GoupedOdds[] => {
+  // Intl formatter for pretty date (system local time)
+  const formatter = new Intl.DateTimeFormat("en-GB", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone, // system local
+  });
+
+  const grouped: Record<string, Odds> = {};
+
+  for (const event of data) {
+    const localDate = new Date(event.commence_time);
+    const formattedDate = formatter.format(localDate); // e.g. "12 September 2025"
+
+    if (!grouped[formattedDate]) grouped[formattedDate] = [];
+    grouped[formattedDate].push(event);
+  }
+
+  return Object.entries(grouped).map(([date, events]) => ({
+    date,
+    num_events: events.length,
+    events,
+  }));
+};
+
+export default transformEvents;
