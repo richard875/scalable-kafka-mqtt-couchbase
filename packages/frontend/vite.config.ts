ddeb0199d14fd5a1f23c9e42c7c8171c7d7edbe2
Richard Everley <hello@richardeverley.com>
2025-09-11 09:05:35 +1000

    - Refactor: Refactor `tsconfig.json` and `vite.config.ts`

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -9,7 +9,8 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@fdj/shared": path.resolve(__dirname, "../shared"),
+      "@fdj/shared/enums": path.resolve(__dirname, "../shared/enums"),
+      "@fdj/shared/types": path.resolve(__dirname, "../shared/types"),
     },
   },
 });
