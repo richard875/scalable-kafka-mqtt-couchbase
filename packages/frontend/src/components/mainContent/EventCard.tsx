c564d385a48fe6bbb89de613de0c71fe78bef1c8
Richard Everley <hello@richardeverley.com>
2025-09-10 16:08:20 +1000

    - Feature: Separate `EventRow.tsx` from `EventCard.tsx` component

--- packages/frontend/src/components/mainContent/EventCard.tsx
+++ packages/frontend/src/components/mainContent/EventCard.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import clock from "@/assets/icons/clock.svg";
+import EventRow from "./EventRow";
 import type { GroupedOdds } from "@/types/groupedOdds";
-import formatIsoToLocalTime from "@/helper/formatIsoToLocalTime";
 
 const EventCard = ({ group }: { group: GroupedOdds }) => {
   const [collapsed, setCollapsed] = useState(false);
@@ -33,51 +32,9 @@ const EventCard = ({ group }: { group: GroupedOdds }) => {
               <div className="w-13"></div>
             </div>
           </div>
-          {group.events.map(event => {
-            const unibet = event.bookmakers.find(bm => bm.key === "unibet");
-            if (!unibet || !unibet.markets.length) return null;
-
-            const homeTeam = event.home_team;
-            const awayTeam = event.away_team;
-            const homeOdds = unibet.markets[0].outcomes.find(outcome => outcome.name === homeTeam);
-            const awayOdds = unibet.markets[0].outcomes.find(outcome => outcome.name === awayTeam);
-            if (!homeOdds || !awayOdds) return null;
-
-            const randomNum = Math.floor(Math.random() * (900 - 100 + 1)) + 100;
-
-            return (
-              <div
-                key={event.id}
-                className="w-full h-14 flex items-center justify-between cursor-pointer border-t border-[#dddddd] bg-white hover:bg-[#f5fffa]"
-              >
-                <div className="flex h-full">
-                  <div className="w-15 h-full border-r border-[#dddddd] flex items-center justify-center">
-                    <span className="text-[#555555] text-xs font-normal">
-                      {formatIsoToLocalTime(event.commence_time)}
-                    </span>
-                  </div>
-                  <div className="flex-1 flex flex-col pl-3 items-start justify-center">
-                    <span className="text-[#333333] text-sm font-normal">{homeTeam}</span>
-                    <span className="text-[#333333] text-sm font-normal">{awayTeam}</span>
-                  </div>
-                </div>
-                <div className="flex items-center gap-2 text-[#555555] text-xs font-normal">
-                  <div className="w-7.5 flex items-center justify-center">
-                    <img src={clock} alt="clock" className="w-3.5 h-3.5" />
-                  </div>
-                  <div className="w-16 h-9 rounded-xs flex items-center justify-center bg-[#147b45] hover:bg-[#00582c]">
-                    <span className="text-white text-sm font-bold">{homeOdds.price}</span>
-                  </div>
-                  <div className="w-16 h-9 rounded-xs flex items-center justify-center bg-[#147b45] hover:bg-[#00582c]">
-                    <span className="text-white text-sm font-bold">{awayOdds.price}</span>
-                  </div>
-                  <div className="w-13 flex items-center justify-center">
-                    <span className="text-[#555555] text-xs font-normal mr-2">+{randomNum}</span>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+          {group.events.map(event => (
+            <EventRow key={event.id} event={event} />
+          ))}
         </>
       )}
     </div>
---
+++ packages/frontend/src/components/mainContent/EventRow.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useMemo } from "react";
+import clock from "@/assets/icons/clock.svg";
+import type { Odds } from "@/types/odds";
+import useSportStore from "@/store/sportStore";
+import formatIsoToLocalTime from "@/helper/formatIsoToLocalTime";
+
+const EventRow = ({ event }: { event: Odds }) => {
+  const { selectedBets, setSelectedBet } = useSportStore();
+
+  useEffect(() => {
+    console.log(selectedBets);
+  }, [selectedBets]);
+
+  const randomNum = useMemo(() => Math.floor(Math.random() * (900 - 100 + 1)) + 100, []);
+
+  const unibet = event.bookmakers.find(bm => bm.key === "unibet");
+  if (!unibet || !unibet.markets.length) return null;
+
+  const homeTeam = event.home_team;
+  const awayTeam = event.away_team;
+  const team = `${homeTeam} - ${awayTeam}`;
+  const homeOdds = unibet.markets[0].outcomes.find(outcome => outcome.name === homeTeam);
+  const awayOdds = unibet.markets[0].outcomes.find(outcome => outcome.name === awayTeam);
+  if (!homeOdds || !awayOdds) return null;
+
+  return (
+    <div
+      key={event.id}
+      className="w-full h-14 flex items-center justify-between cursor-pointer border-t border-[#dddddd] bg-white hover:bg-[#f5fffa]"
+    >
+      <div className="flex h-full">
+        <div className="w-15 h-full border-r border-[#dddddd] flex items-center justify-center">
+          <span className="text-[#555555] text-xs font-normal">
+            {formatIsoToLocalTime(event.commence_time)}
+          </span>
+        </div>
+        <div className="flex-1 flex flex-col pl-3 items-start justify-center">
+          <span className="text-[#333333] text-sm font-normal">{homeTeam}</span>
+          <span className="text-[#333333] text-sm font-normal">{awayTeam}</span>
+        </div>
+      </div>
+      <div className="flex items-center gap-2 text-[#555555] text-xs font-normal">
+        <div className="w-7.5 flex items-center justify-center">
+          <img src={clock} alt="clock" className="w-3.5 h-3.5" />
+        </div>
+        <div
+          onClick={() => setSelectedBet({ ...homeOdds, team })}
+          className="w-16 h-9 rounded-xs flex items-center justify-center bg-[#147b45] hover:bg-[#00582c]"
+        >
+          <span className="text-white text-sm font-bold">{homeOdds.price}</span>
+        </div>
+        <div
+          onClick={() => setSelectedBet({ ...awayOdds, team })}
+          className="w-16 h-9 rounded-xs flex items-center justify-center bg-[#147b45] hover:bg-[#00582c]"
+        >
+          <span className="text-white text-sm font-bold">{awayOdds.price}</span>
+        </div>
+        <div className="w-13 flex items-center justify-center">
+          <span className="text-[#555555] text-xs font-normal mr-2">+{randomNum}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EventRow;
