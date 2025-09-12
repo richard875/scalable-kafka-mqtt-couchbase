7d7dd05c767a374a905632a9cdfef58a81b22ce1
Richard Everley <hello@richardeverley.com>
2025-09-12 22:56:16 +1000

    - Deploy: Update `MQTT_BROKER_URL` and `MQTT_URL` to use env var

--- packages/notification-service/src/constants.ts
+++ packages/notification-service/src/constants.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const MQTT_URL = process.env.MQTT_URL!;
+export const MQTT_URL =
+  process.env.MQTT_BROKER_URL || process.env.MQTT_URL || "mqtt://localhost:1883";
