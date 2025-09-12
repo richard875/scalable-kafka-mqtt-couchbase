3b16252be40de1829102dd7f260f2db036b243a9
Richard Everley <hello@richardeverley.com>
2025-09-12 16:23:49 +1000

    - Refactor: Follow best practice casing and naming rule for `audit-service/src/constants`

--- packages/audit-service/src/constants.ts
+++ packages/audit-service/src/constants.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const clusterConnStr = process.env.COUCHBASE_URL!;
-export const username = process.env.COUCHBASE_USERNAME!;
-export const password = process.env.COUCHBASE_PASSWORD!;
-export const bucketName = process.env.COUCHBASE_BUCKET!;
+export const COUCHBASE_URL = process.env.COUCHBASE_URL!;
+export const COUCHBASE_USERNAME = process.env.COUCHBASE_USERNAME!;
+export const COUCHBASE_PASSWORD = process.env.COUCHBASE_PASSWORD!;
+export const COUCHBASE_BUCKET = process.env.COUCHBASE_BUCKET!;
--- packages/audit-service/src/services/auditLogService.ts
+++ packages/audit-service/src/services/auditLogService.ts
@@ -1,24 +1,30 @@
 import couchbase, { Bucket, Collection, Cluster } from "couchbase";
 import type SlipItem from "@fdj/shared/types/slipItem";
-import { clusterConnStr, username, password, bucketName } from "@audit-service/constants.js";
+import {
+  COUCHBASE_URL,
+  COUCHBASE_USERNAME,
+  COUCHBASE_PASSWORD,
+  COUCHBASE_BUCKET,
+} from "@audit-service/constants.js";
 
 const initCouchbase = async (): Promise<Collection> => {
-  const couchbaseClient: Cluster = await couchbase.connect(clusterConnStr, { username, password });
+  const couchbaseCredentials = { username: COUCHBASE_USERNAME, password: COUCHBASE_PASSWORD };
+  const couchbaseClient: Cluster = await couchbase.connect(COUCHBASE_URL, couchbaseCredentials);
   const bucketMgr = couchbaseClient.buckets();
 
   try {
-    await bucketMgr.createBucket({ name: bucketName, ramQuotaMB: 100, flushEnabled: true });
-    console.log(`✅ Bucket '${bucketName}' created`);
+    await bucketMgr.createBucket({ name: COUCHBASE_BUCKET, ramQuotaMB: 100, flushEnabled: true });
+    console.log(`✅ Bucket '${COUCHBASE_BUCKET}' created`);
   } catch (err) {
     if (err instanceof couchbase.BucketExistsError) {
-      console.log(`⚠️ Bucket '${bucketName}' already exists, skipping creation`);
+      console.log(`⚠️ Bucket '${COUCHBASE_BUCKET}' already exists, skipping creation`);
     } else throw err;
   }
 
-  const bucket: Bucket = couchbaseClient.bucket(bucketName);
+  const bucket: Bucket = couchbaseClient.bucket(COUCHBASE_BUCKET);
   const collection: Collection = bucket.defaultCollection();
 
-  console.log(`📦 Couchbase initialized, bucket: ${bucketName}`);
+  console.log(`📦 Couchbase initialized, bucket: ${COUCHBASE_BUCKET}`);
   return collection;
 };
 
