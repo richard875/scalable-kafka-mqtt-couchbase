12e37711de6eed11c76bd7d2f22471e390037b45
Richard Everley <hello@richardeverley.com>
2025-09-14 10:29:16 +1000

    - Feature: Add loading state in `frontend`

--- packages/frontend/src/components/mainContent/MainContent.tsx
+++ packages/frontend/src/components/mainContent/MainContent.tsx
@@ -11,9 +11,13 @@ const MainContent = () => {
       <div className="mt-5 mb-4 select-none">
         <span className="text-[#555555] text-base font-medium">Live & Upcoming</span>
       </div>
-      {groupedOdds.map(group => (
-        <EventCard key={group.date} group={group} />
-      ))}
+      {groupedOdds.length === 0 ? (
+        <div className="w-full mt-25 flex items-center justify-center">
+          <div className="spinner-loader" />
+        </div>
+      ) : (
+        groupedOdds.map(group => <EventCard key={group.date} group={group} />)
+      )}
     </>
   );
 };
