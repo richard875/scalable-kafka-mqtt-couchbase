cc490a55ae67b886561fad88729b3f61366617f8
Richard Everley <hello@richardeverley.com>
2025-09-20 22:36:22 +1000

    - Logging: Add Console Warning for Schema Client

--- packages/frontend/src/helper/betVersionValidation.ts
+++ packages/frontend/src/helper/betVersionValidation.ts
@@ -3,7 +3,11 @@ import BetV1 from "@fdj/shared/types/kafka/betV1";
 const VALID_VERSION = "v1";
 
 const betVersionValidation = (payload: BetV1): string | null => {
-  if (payload.version !== VALID_VERSION) return null;
+  if (payload.version !== VALID_VERSION) {
+    const warningMsg = `Unsupported bet version: ${payload.version}, expected: ${VALID_VERSION} - ignoring message.`;
+    console.warn(warningMsg);
+    return null;
+  }
 
   const message = payload.payload;
   return `Bet placed on ${message.team}: $${message.amount} on ${message.name} at odds of ${message.price} 🎯`;
