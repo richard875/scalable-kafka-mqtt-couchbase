172b274594ee652284f5509770155c30f1907a26
Richard Everley <hello@richardeverley.com>
2025-09-22 07:20:05 +1000

    - Deploy: Add `FROM_EMAIL` and `TO_EMAIL` in `docker-compose.production.yml` and `email-service` Constants

--- packages/email-service/src/constants.ts
+++ packages/email-service/src/constants.ts
@@ -3,3 +3,5 @@ dotenv.config();
 
 export const REDIS_HOSTS = process.env.REDIS_HOSTS!;
 export const RESEND_API_KEY = process.env.RESEND_API_KEY!;
+export const FROM_EMAIL = process.env.FROM_EMAIL!;
+export const TO_EMAIL = process.env.TO_EMAIL!;
