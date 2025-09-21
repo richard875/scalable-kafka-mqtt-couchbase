6608e08f940232ef79c1c645db24cd3ba687e251
Richard Everley <hello@richardeverley.com>
2025-09-22 07:20:33 +1000

    - Feature: Add and implement send email service

--- packages/email-service/src/helper/processBets.ts
+++ packages/email-service/src/helper/processBets.ts
@@ -1,21 +1,26 @@
 import redisService from "../services/redisService.js";
 import type BetV2 from "@fdj/shared/types/kafka/betV2.js";
 import { listKey, ttlKey } from "../helper/formatKeys.js";
+import sendBetslipEmail from "../services/emailService.js";
 
 const processBets = async (userId: string): Promise<void> => {
-  const key = listKey(userId);
-  const ttl = ttlKey(userId);
+  try {
+    const key = listKey(userId);
+    const ttl = ttlKey(userId);
 
-  const pipeline = redisService.multi();
-  pipeline.lRange(key, 0, -1);
-  pipeline.del(key);
-  pipeline.del(ttl);
+    const pipeline = redisService.multi();
+    pipeline.lRange(key, 0, -1);
+    pipeline.del(key);
+    pipeline.del(ttl);
 
-  const [bets] = (await pipeline.exec()) as [string[], unknown];
-  if (!bets || bets.length === 0) throw new Error("Failed to execute Redis pipeline");
+    const [bets] = (await pipeline.exec()) as [string[], unknown];
+    if (!bets || bets.length === 0) throw new Error("Failed to execute Redis pipeline");
 
-  const parsedBets = bets.map(bet => JSON.parse(bet) as BetV2);
-  console.log(JSON.stringify(parsedBets, null, 2));
+    const parsedBets = bets.map(bet => JSON.parse(bet) as BetV2);
+    await sendBetslipEmail(parsedBets);
+  } catch (error) {
+    console.error("Error processing bets:", error);
+  }
 };
 
 export default processBets;
---
+++ packages/email-service/src/services/emailService.tsx
@@ -0,0 +1,23 @@
+import { Resend } from "resend";
+import type BetV2 from "@fdj/shared/types/kafka/betV2.js";
+import { RESEND_API_KEY, FROM_EMAIL, TO_EMAIL } from "../constants.js";
+import NotificationEmail from "@email-service/templates/NotificationEmail.js";
+
+const sendBetslipEmail = async (bets: BetV2[]) => {
+  try {
+    const resend = new Resend(RESEND_API_KEY);
+
+    const { error } = await resend.emails.send({
+      from: FROM_EMAIL,
+      to: [TO_EMAIL],
+      subject: "Your Betslip from Unibet ⚽️",
+      react: <NotificationEmail companyName="Unibet" bets={bets} />,
+    });
+
+    if (error) throw new Error("Failed to send email");
+  } catch (error) {
+    console.error("Error sending betslip email:", error);
+  }
+};
+
+export default sendBetslipEmail;
